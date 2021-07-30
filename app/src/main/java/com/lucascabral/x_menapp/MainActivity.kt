33a182075acee4053d3a111ed6eccc2ac8a4d5d1
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-07-30 11:59:23 -0300

    navigation configurado.

--- app/src/main/java/com/lucascabral/x_menapp/MainActivity.kt
+++ app/src/main/java/com/lucascabral/x_menapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.lucascabral.x_menapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.xmen_nav_graph)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
