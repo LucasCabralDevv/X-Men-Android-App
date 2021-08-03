66892e6fd5bd488d1c2d2808e83781a676c4c8e9
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-08-02 22:09:26 -0300

    Navigation excluído.

--- app/src/main/java/com/lucascabral/x_menapp/ui/view/CharactersActivity.kt
+++ app/src/main/java/com/lucascabral/x_menapp/ui/view/CharactersActivity.kt
@@ -1,35 +1,26 @@
 package com.lucascabral.x_menapp.ui.view
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
-import com.lucascabral.x_menapp.databinding.FragmentCharactersBinding
+import com.lucascabral.x_menapp.databinding.ActivityCharactersBinding
 import com.lucascabral.x_menapp.ui.adapter.CharactersAdapter
 import com.lucascabral.x_menapp.ui.viewmodel.CharactersViewModel
 import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class CharactersFragment : Fragment() {
+class CharactersActivity : AppCompatActivity() {
 
     private val viewModel: CharactersViewModel by viewModel()
-    private lateinit var binding: FragmentCharactersBinding
+    private lateinit var binding: ActivityCharactersBinding
     private lateinit var charactersAdapter: CharactersAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentCharactersBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCharactersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initRecyclerView()
         initViewModel()
